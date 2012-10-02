cb022863b6102ede99d750b4106c8bf99fb3a20e
Omar Ontiveros <netadmin@lafe-ep.org>
2012-10-02 14:43:49 -0600

    CHM Poster

--- app/assets/javascripts/omareo.js
+++ app/assets/javascripts/omareo.js
@@ -1,14 +1,14 @@
 // Custom Functions Go Here
 
 (function() { // Create local namespace
-    var banners = $$('.banner'); // no need to create an empty array first
-    banners.invoke('hide');      // no need to use each
+    var banners = $$('.banner'); 
+    banners.invoke('hide');      
 
     function showNextBanner() {
-        banners.last().hide();            // hide previous banner
-        banners.first().show();           // show next banner
-        banners.push(banners.shift());    // first element moves to become last element
-        showNextBanner.delay(7);          // set a timeout to show the next banner
+        banners.last().hide();           
+        banners.first().show();           
+        banners.push(banners.shift());    
+        showNextBanner.delay(7);          
     }
 
     showNextBanner(); // start the banner rotation/looping
