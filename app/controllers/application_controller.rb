0fe12e6a4a443e0b93801962e7714437243405f5
misoo <netadmin@lafe-ep.org>
2013-02-14 11:21:10 -0700

    stylesheet stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+	
+	before_filter :notHomePage
+	
+	def notHomePage
+		@container ="container"
+	end
 end
